28ba38b8737446d4661e4a305623310af98250b1
Will Simpson <will@wjrs.co.uk>
2015-06-09 17:07:51 +0100

    Added license.

--- src/main/java/org/orcid/orikademo/formatB/InterestsB.java
+++ src/main/java/org/orcid/orikademo/formatB/InterestsB.java
@@ -1,3 +1,19 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2015 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
 package org.orcid.orikademo.formatB;
 
 import java.util.ArrayList;
